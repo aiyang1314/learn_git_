71ae05a005bf29fb5d28d1279035cb36f9b00085
aiyang1314 <1207809538@qq.com>
2022-05-03 16:01:18 +0800

    额

---
+++ js-workspace/node/node_js/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+//创建应用对象
+const app = express();
+//创建路由规则
+app.get("/", (request, response) => {
+    //设置响应
+    response.send("hello express");
+});
+//监听端口启动服务
+app.listen(8000, () => {
+    console.log('服务已经启动，8000端口监听中...');
+});
