c5618a73279034c72a3a8419cd5de8733639beb6
aiyang1314 <1207809538@qq.com>
2022-05-03 16:02:31 +0800

    额

--- js-workspace/js/index.js
+++ js-workspace/js/index.js
@@ -1,13 +1,25 @@
-let fs = require("fs");
-fs.open("hello.txt", "w", function (err, fd) {
-    if (!err) {
-        console.log("打开成功");
-        fs.write(fd, "第一次", function (err) {
-            if (!err) {
-                console.log("写入成功");
-            }
-        })
-    } else {
-        console.log(err);
-    }
-})
+// let fs = require("fs");
+// fs.open("hello.txt", "w", function (err, fd) {
+//     if (!err) {
+//         console.log("打开成功");
+//         fs.write(fd, "第二次", function (err) {
+//             if (!err) {
+//                 console.log("写入成功");
+//             }
+//         })
+//     } else {
+//         console.log(err);
+//     }
+// })
+const express = require("express");
+//创建应用对象
+const app = express();
+//创建路由规则
+app.get("/", (request, response) => {
+    //设置响应
+    response.send("hello express");
+});
+//监听端口启动服务
+app.listen(8000, () => {
+    console.log('服务已经启动，8000端口监听中...');
+});
