3abcc95ddc9527178ff36094799e313cdb8492e3
aiyang1314 <1207809538@qq.com>
2022-05-03 17:25:42 +0800

    写了一个AJAX入门

--- js-workspace/node/js/server.js
+++ js-workspace/node/js/server.js
@@ -11,15 +11,20 @@
 //         console.log(err);
 //     }
 // })
+//引入express模块
 const express = require("express");
 //创建应用对象
 const app = express();
 //创建路由规则
-app.get("/", (request, response) => {
-    //设置响应
-    response.send("hello express");
+//request 是对请求报文的封装
+//response 是对响应报文的封装
+app.get("/server", (request, response) => {
+    //设置响应头 设置允许跨域
+    response.setHeader("Access-Control-Allow-Origin", '*');
+    //设置响应体
+    response.send("hello AJAX");
 });
 //监听端口启动服务
 app.listen(8000, () => {
-    console.log('服务已经启动，8000端口监听中...');
+    console.log('服务已经启动,8000端口监听中...');
 });
